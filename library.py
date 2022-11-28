deaa41ccf0381b7335c81c54b679a8bb3265a808
Christopher Cialone <ccialone0929@gmail.com>
2022-11-28 07:29:14 -0500

    filled in the appropriate fields, working out the kinks

--- library.py
+++ library.py
@@ -4,19 +4,31 @@ from book import Book
 class Library():
     def __init__(self):
         """Initialize the empty book list"""
-        pass
+        self.books = []
+
+  
 
     def add_title(self):
         """Add a Book object with the given title and author to the book list"""
-        pass
+        self.title = title
+        self.author = author
+        book = Book(title, author)
+        self.books.append(book)
 
     def count_books(self):
         """Return the number of books currently in the booklist"""
-        pass
+        return len(self.books)
 
     def remove_title(self):
         """Remove a book from the book list"""
-        pass
+        self.title = title
+        for booktitle in self.books:
+            if booktitle.title == self.title:
+                self.books.remove(booktitle)
+
+    def display_books(self):
+        for book in sorted(self.books):
+            print(book)
 
     def is_empty(self):
         """Return True if the book list is empty, False if not"""
